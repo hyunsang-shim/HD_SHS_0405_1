3d39cf2309e3e2ace37be52dbdb6fdf343e5e08b
hyunsang-shim <shimhyunsang.dev@gmail.com>
2022-04-17 16:04:30 +0900

    문제 1 풀이

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,28 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[,] arr = new int[5, 5];
+
+            int num = 1;
+            
+            for(int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    arr[i, j] = num++;
+                }
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write(arr[i, j].ToString() + '\t');
+                }
+
+                Console.Write("\n");
+            }
+
         }
     }
 }
